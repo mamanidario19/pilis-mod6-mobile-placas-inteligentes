13c185ee5b49b94566f6904aa66a0e33b7bf87a9
mamanidario19 <mamanidario.19@gmail.com>
2023-09-16 04:19:16 -0300

    scanner styles

--- src/screens/ScannerScreen/ScannerScreen.jsx
+++ src/screens/ScannerScreen/ScannerScreen.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button, Linking } from "react-native";
+import { Text, View, StyleSheet, Button, Linking, Modal, TouchableOpacity } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import { COLORS, FONT_SIZE, SPACING } from '../../utils/theme'
 
 export const ScannerScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [scannedData, setScannedData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -16,25 +18,33 @@ export const ScannerScreen = () => {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    setScannedData(data); // Guarda los datos del codigo QR escaneado
-    handleLinking(data); // Abre direccionr usando Linking
+    setScannedData(data); // Guarda los datos del código QR escaneado
+    setShowModal(true); // Abre el modal para mostrar los datos
   };
 
-  const handleLinking = (data) => {
-    // Verificamos si los datos son una url o link valido
-    if (Linking.canOpenURL(data)) {
-      Linking.openURL(data).catch((err) => console.error('Error al abrir la URL: ', err));
-    } else {
-      console.warn('Los datos escaneados no son una URL válida.');
+  const closeModal = () => {
+    setShowModal(false);
+    setScanned(false); // Restablece el estado del escaneo
+  };
+
+  // Función para intentar analizar manualmente los datos JSON
+  const parseJsonData = (data) => {
+    try {
+      return JSON.parse(data);
+    } catch (error) {
+      console.error("Error al analizar los datos JSON:", error);
+      return null;
     }
   };
 
+  const parsedData = parseJsonData(scannedData);
+
   if (hasPermission === null) {
-    return <Text>Requesting for Camera Permission</Text>
+    return <Text>Requesting for Camera Permission</Text>;
   }
 
   if (hasPermission === false) {
-    return <Text>No Access to Camera</Text>
+    return <Text>No Access to Camera</Text>;
   }
 
   return (
@@ -43,20 +53,87 @@ export const ScannerScreen = () => {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && (
-        <View>
-          <Text>Escaneado: {scannedData}</Text>
-          <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />
+
+      <Modal visible={showModal} transparent={true} animationType="fade">
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Detalle de Escaneo</Text>
+            {parsedData && (
+              <>
+                <View style={styles.dataContainer}>
+                  <Text style={styles.dataLabel}>Nombre:</Text>
+                  <Text style={styles.dataValue}>{parsedData.name || "N/A"}</Text>
+                </View>
+                <View style={styles.dataContainer}>
+                  <Text style={styles.dataLabel}>Género:</Text>
+                  <Text style={styles.dataValue}>{parsedData.gender || "N/A"}</Text>
+                </View>
+                <View style={styles.dataContainer}>
+                  <Text style={styles.dataLabel}>Vacunas:</Text>
+                  <Text style={styles.dataValue}>{parsedData.vacunas || "N/A"}</Text>
+                </View>
+                <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
+                  <Text style={styles.buttonText}>Volver</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
         </View>
-      )}
+      </Modal>
+
+      {/*scanned && (
+        <Button style={styles.closeButton} title="Escanear de nuevo" onPress={() => setScanned(false)} />
+      )*/}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center'
-  }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: SPACING.md,
+    borderRadius: 10,
+    width: '85%',
+  },
+  modalTitle: {
+    fontSize: FONT_SIZE.lg,
+    fontWeight: 'bold',
+    marginBottom: SPACING.md,
+  },
+  dataContainer: {
+    marginTop: SPACING.xs,
+    alignItems: 'center',
+  },
+  dataLabel: {
+    fontSize: FONT_SIZE.md,
+    fontWeight: 'bold',
+  },
+  dataValue: {
+    fontSize: FONT_SIZE.md,
+    marginTop: SPACING.sm,
+  },
+  closeButton: {
+    backgroundColor: COLORS.primary,
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: FONT_SIZE.md,
+  },
+});
